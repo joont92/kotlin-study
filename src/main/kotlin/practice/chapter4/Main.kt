1d67e56733a0e92d7129dda9723cf913d7b4cba7
pj.ark <pj.ark@kakaocorp.com>
2021-07-15 08:38:24 +0900

    practice chapter4

--- src/main/kotlin/practice/chapter4/Main.kt
+++ src/main/kotlin/practice/chapter4/Main.kt
@@ -6,6 +6,17 @@ fun main() {
 
     println(Num(10))
     Sum(Num(10), Num(20))
+
+    println(Student("NOT TEST", 10).name)
+    Student2("JOONT")
+    Student2("JOONT", 10)
+
+    val v1 = VO("joont", 30)
+    val v2 = VO("joont", 30)
+    v2.sex = 'F'
+    println(v1 == v2)
+
+    CompanionUser.newFacebookUser(1)
 }
 
 interface Clickable {
@@ -59,3 +70,84 @@ internal fun TalkativeButton.giveSpeech() {
 sealed class Expr
 class Num(val value: Int): Expr()
 class Sum(val left: Expr, val right: Expr): Expr()
+
+class Student(_name: String, val age: Int) {
+    var name: String
+
+    init {
+        name = _name
+    }
+
+    init {
+        name = "TEST"
+    }
+}
+
+class Student2 {
+    // 생성자에서 무조건 초기화 해야함
+    val name: String
+    val age: Int
+
+    constructor(_name: String, _age: Int) {
+        name = _name
+        age = _age
+    }
+
+    constructor(_name: String) {
+        name = _name
+        age = 10
+    }
+
+    init {
+        println("test")
+    }
+}
+
+interface User {
+    val nickname: String
+}
+
+class PrivateUser(override val nickname: String): User
+
+class SubscribingUser(val email: String): User {
+    override val nickname: String
+        get() = email.substringBefore("@")
+}
+
+class FacebookUser(val accountId: Int): User {
+    override val nickname: String = accountId.toString()
+}
+
+class LengthCounter {
+    var counter: Int = 0
+        private set
+
+    fun addWord(word: String) {
+        counter += word.length
+    }
+}
+
+data class VO(val name: String, val age: Int) {
+    var sex: Char = 'M'
+}
+
+class DelegatingCollection<T>(
+    private val innerList: Collection<T> = ArrayList()
+): Collection<T> by innerList {
+    fun concatSize(collection: Collection<T>) = innerList.size + collection.size
+}
+
+class CompanionUser private constructor(val nickname: String){
+
+    companion object {
+        val NICKNAME_PREFIX = "NICK_"
+
+        fun newSubscribingUser(email: String) {
+            CompanionUser(NICKNAME_PREFIX + email.substringBefore("@"))
+        }
+
+        fun newFacebookUser(facebookAccountId: Int) {
+            CompanionUser(NICKNAME_PREFIX + facebookAccountId.toString())
+        }
+    }
+}
